90c14bbb15d7f4aaff7104b61cc58c3aaf602544
Taku1995 <y.takuto.jp@gmail.com>
2021-06-04 06:38:12 +0000

    update

---
+++ js/script.js
@@ -0,0 +1,100 @@
+//アルバムデータの作成
+$(function(){
+  //最初はポップアップ非表示
+  $(".popup").hide();
+  
+  $(".btn-prof").on("click",() =>{
+     //ポップアップが開く処理
+     $(".popup-work").fadeIn();
+     $("#content-wrapper").css("background","#9e9e9e82");
+  })
+  
+  $(".btn-const").on("click",() =>{
+     //ポップアップが開く処理
+     $(".popup-constract").fadeIn();
+     $("#content-wrapper").css("background","#9e9e9e82");
+  })  
+  
+    $(".btn-dent").on("click",() =>{
+     //ポップアップが開く処理
+     $(".popup-dental").fadeIn();
+     $("#content-wrapper").css("background","#9e9e9e82");
+  })
+  
+  $(".btn").on("click",() =>{
+     //ポップアップが閉じる処理
+     $(".popup").fadeOut();
+     $("#content-wrapper").css("background","url(images/top-back.png)");
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
