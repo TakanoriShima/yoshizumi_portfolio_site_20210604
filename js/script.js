748f1aa48dfd3cafef558857e6efb2e6d5908cf6
TakanoriShima <quark2galaxy@gmail.com>
2021-06-04 22:58:46 +0900

    modified by shima

--- js/script.js
+++ js/script.js
@@ -1,32 +1,50 @@
-//アルバムデータの作成
+/* global $*/
 $(function(){
-  //最初はポップアップ非表示
-  $(".popup").hide();
+    $(".popup").hide();
+    
+    $('img').on('click', function(){
+        const work = $(this).data('work');
+        $('#' + work).fadeIn();
+        $("#content-wrapper").css("background","#9e9e9e82");
+    })
+    
+    $(".btn").on("click",() =>{
+        //ポップアップが閉じる処理
+        $(".popup").fadeOut();
+        $("#content-wrapper").css("background","url(images/top-back.png)");
+    })
+    
+});
+
+// //アルバムデータの作成
+// $(function(){
+//   //最初はポップアップ非表示
+//   $(".popup").hide();
   
-  $(".btn-prof").on("click",() =>{
-     //ポップアップが開く処理
-     $(".popup-work").fadeIn();
-     $("#content-wrapper").css("background","#9e9e9e82");
-  })
+//   $(".btn-prof").on("click",() =>{
+//      //ポップアップが開く処理
+//      $(".popup-work").fadeIn();
+//      $("#content-wrapper").css("background","#9e9e9e82");
+//   })
   
-  $(".btn-const").on("click",() =>{
-     //ポップアップが開く処理
-     $(".popup-constract").fadeIn();
-     $("#content-wrapper").css("background","#9e9e9e82");
-  })  
+//   $(".btn-const").on("click",() =>{
+//      //ポップアップが開く処理
+//      $(".popup-constract").fadeIn();
+//      $("#content-wrapper").css("background","#9e9e9e82");
+//   })  
   
-    $(".btn-dent").on("click",() =>{
-     //ポップアップが開く処理
-     $(".popup-dental").fadeIn();
-     $("#content-wrapper").css("background","#9e9e9e82");
-  })
+//     $(".btn-dent").on("click",() =>{
+//      //ポップアップが開く処理
+//      $(".popup-dental").fadeIn();
+//      $("#content-wrapper").css("background","#9e9e9e82");
+//   })
   
   $(".btn").on("click",() =>{
      //ポップアップが閉じる処理
      $(".popup").fadeOut();
      $("#content-wrapper").css("background","url(images/top-back.png)");
   })
-});
+// });
 
 
 
--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,24 @@
 /* global $*/
 $(function(){
+    
     //画像スライド
     $("#effect img").eq(1).css("marginLeft","-1000px")
     $("#effect img").eq(2).css("marginLeft","-1000px")
     $("#effect img").eq(3).css("marginLeft","-1000px")
+    
     let count_slider = 0;
+    
     //画像スライド関数
     const slider = () => {
-    $("#effect img").eq(count_slider % 4).animate({marginLeft:"1000px"},1000,function(){
-    $("#effect img").eq(count_slider % 4).css("marginLeft","-1000px");
-    count_slider++;
-    
-
-    
-    });
-    $("#effect img").eq((count_slider +1) % 4).animate({marginLeft:"0"},1000);
-    
+        $("#effect img").eq(count_slider % 4).animate({marginLeft:"1000px"},1000,function(){
+            $("#effect img").eq(count_slider % 4).css("marginLeft","-1000px");
+            count_slider++;
+        });
+        
+        $("#effect img").eq((count_slider + 1) % 4).animate({marginLeft:"0"},1000);
+        
     }
     
     setInterval(slider,5000);
-        
-
 });
 
